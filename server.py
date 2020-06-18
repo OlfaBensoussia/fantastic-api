797bf12c68747970dcc6ab6b9720e6a9f55c2e89
OlfaBensoussia <olfa.bensoussia@ensi-uma.tn>
2020-06-18 13:40:06 +0200

    Revert "suggested solution to improve searching complexity to O(1)"
    This reverts commit 6ecd0bc52b46f9ce311024d985a00dbb581458dc.

--- server.py
+++ server.py
@@ -26,31 +26,29 @@ todo = api.model(
 class TodoDAO(object):
     def __init__(self):
         self.counter = 0
-        # We will use a dictionnary (Hashmap built-in in Python) : the key will be the id and the value will be the task
-        self.todos = {}
-      
+        self.todos = []
 
     def get(self, id):
-        Todo = {}
-        Todo['id'] = id
-        Todo['task'] = self.todos[id]  
-        return Todo   
+        for todo in self.todos:
+            # TODO : Improve the searching complexity to O(1) using hashmap structure
+            if todo['id'] == id:
+                return todo
         api.abort(404, "Todo {} doesn't exist".format(id))
 
     def create(self, data):
         todo = data
-        todo['id'] = id = self.counter = self.counter + 1
-        todo['task'] = self.todos[id] = data['task'] 
+        todo['id'] = self.counter = self.counter + 1
+        self.todos.append(todo)
         return todo
 
     def update(self, id, data):
-        todo = data 
-        todo['id'] = id
-        todo['task'] = self.todos[id] = data['task']
+        todo = self.get(id)
+        todo.update(data)
         return todo
 
     def delete(self, id):
-        del self.todos[id]
+        todo = self.get(id)
+        self.todos.remove(todo)
 
 
 DAO = TodoDAO()
@@ -63,7 +61,7 @@ DAO.create({'task': 'profit!'})
 class TodoList(Resource):
     '''Shows a list of all todos, and lets you POST to add new tasks'''
     @ns.doc('list_todos')
-    #@ns.marshal_list_with(todo)
+    @ns.marshal_list_with(todo)
     def get(self):
         '''List all tasks'''
         return DAO.todos
@@ -82,7 +80,7 @@ class TodoList(Resource):
 class Todo(Resource):
     '''Show a single todo item and lets you delete them'''
     @ns.doc('get_todo')
-    #@ns.marshal_with(todo)
+    @ns.marshal_with(todo)
     def get(self, id):
         '''Fetch a given resource'''
         return DAO.get(id)
