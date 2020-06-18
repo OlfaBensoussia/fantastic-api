2d318ea32fea5d2dfe8dca857d1acd2f71cac89b
OlfaBensoussia <olfa.bensoussia@ensi-uma.tn>
2020-06-18 13:22:54 +0200

    Revert "Fix typo and add add exceptions for empty and not found tasks"
    This reverts commit ba6e1e3285c9c6acf5e6ca8b46937e4f3ca07142.

--- test_server.py
+++ test_server.py
@@ -1,5 +1,4 @@
 from server import TodoDAO
-import pytest
 
 
 def test_list_todo():
@@ -12,25 +11,11 @@ def test_list_todo():
     assert t2['task'] == t1['task']
 
 
+def test_empty_todo():
+    DAO = TodoDAO()
+    assert len(DAO.todos) == 0
+
+
 # TODO : add test raise exception when task is absent
-# If the task is empty
-def empty_todo():
-    DAO = TodoDAO()
-    empty = DAO.create({'task': ''})
-    if not empty['task']:
-        raise Exception("Task is empty")
-
-
-# If the task is null
-def notfound_todo():
-    DAO = TodoDAO()
-    none = DAO.create({'task': None})
-    if none['task'] is None:
-        raise Exception("Task not found")
-
-
 def test_notfound_todo():
-    with pytest.raises(Exception):
-        empty_todo()
-    with pytest.raises(Exception):
-        notfound_todo()
+    pass
